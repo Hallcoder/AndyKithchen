3e3d204f2eaf099a692f19361ad9a3c1ff48d581
Hallcoder <hallcoder25@gmail.com>
2022-07-22 10:20:30 +0200

    last commit with ability to resolve a bug using object destructuring

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import * as actions from './actionTypes'
 import App from './App'
+import {bugAdded} from './actionsCreator'
 import './index.css'
 import store from './store';
+import * as actions from "./actionTypes";
 const unsubscribe = store.subscribe(()=>{
   console.log('Store changed',store.getState());
 })
+store.dispatch(bugAdded('Bug added broo'));
 store.dispatch({
-  type:actions.BUG_ADDED,
+  type:actions.BUG_RESOLVED,
   payload:{
-    description:'bug1'
-  }
-});
-
-unsubscribe()
-store.dispatch({
-  type:actions.BUG_REMOVED,
-  payload:{
-    id:1,
+    id:1
   }
 })
+// store.dispatch({
+//   type:actions.BUG_REMOVED,
+//   payload:{
+//     id:1,
+//   }
+// })
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    {console.log(store.getState())}
