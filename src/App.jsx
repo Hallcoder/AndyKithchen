8b847d32cecc1a8249ea9d36fecf44266851cb2a
Hallcoder <hallcoder25@gmail.com>
2022-07-22 10:02:32 +0200

    created store , action ,  reducer and dispatched some actions on the store

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,7 @@ import { compose, pipe } from "lodash/fp";
 import { produce } from "immer";
 import "./App.css";
 function App() {
-  let book = { title: "Harry Potter" };
-  let updated = publish(book);
-  console.log(book);
-  console.log(updated);
+  
 }
-function publish(book) {
- return produce(book, draftBook => {
-    draftBook.isPublished = true;
-  });
-}
+
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import * as actions from './actionTypes'
 import App from './App'
 import './index.css'
+import store from './store';
+const unsubscribe = store.subscribe(()=>{
+  console.log('Store changed',store.getState());
+})
+store.dispatch({
+  type:actions.BUG_ADDED,
+  payload:{
+    description:'bug1'
+  }
+});
 
+unsubscribe()
+store.dispatch({
+  type:actions.BUG_REMOVED,
+  payload:{
+    id:1,
+  }
+})
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+   {console.log(store.getState())}
     <App />
   </React.StrictMode>
 )
